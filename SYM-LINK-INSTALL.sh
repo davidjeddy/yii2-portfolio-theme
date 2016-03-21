c183b65a74e8d78e646b78cecbbef4d7e00852d3
David E <me@davidjeddy.com>
2016-03-22 06:40:25 +0900

    UPDATED ./SYM-LINL-INSTALL.sh logic.

--- SYM-LINK-INSTALL.sh
+++ SYM-LINK-INSTALL.sh
@@ -1,6 +1,6 @@
-#!
+#!/usr/bin/bash
 if test -d ./frontend/web/themes; then
     echo 'Symlink for yii2-portfolio-theme already exists ';
 else
-    mkdir frontend/web/themes && ln -sfn ./vendor/davidjeddy/yii2-portfolio-theme/src frontend/web/themes;
+    ln -sfn ../../vendor/davidjeddy/yii2-portfolio-theme/src ./frontend/web/theme;
 fi
